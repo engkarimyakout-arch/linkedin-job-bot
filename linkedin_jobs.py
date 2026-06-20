813c549cd83a37cba6af31e948dd99ef0d455df0
engkarimyakout-arch <eng.karim.yakout@gmail.com>
2026-06-20 17:37:04 +0300

    Update linkedin_jobs.py

--- linkedin_jobs.py
+++ linkedin_jobs.py
@@ -1,45 +1,15 @@
-from linkedin_jobs_scraper import LinkedinScraper
-from linkedin_jobs_scraper.events import Events
-from linkedin_jobs_scraper.query import Query
-import requests
 import os
+import requests
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
 
-def send_telegram(msg):
-    requests.post(
-        f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
-        json={
-            "chat_id": CHAT_ID,
-            "text": msg
-        }
-    )
+requests.post(
+    f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
+    json={
+        "chat_id": CHAT_ID,
+        "text": "GitHub Action is working successfully 🚀"
+    }
+)
 
-def on_data(data):
-
-    msg = f"""
-📌 {data.title}
-
-🏢 {data.company}
-
-📍 {data.location}
-
-🔗 {data.link}
-"""
-
-    send_telegram(msg)
-
-scraper = LinkedinScraper()
-
-scraper.on(Events.DATA, on_data)
-
-queries = [
-    Query(
-        query="Data Engineer",
-        locations=["United Arab Emirates"],
-        limit=10
-    )
-]
-
-scraper.run(queries)
+print("Message sent")
